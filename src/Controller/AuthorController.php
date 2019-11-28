49ce09bc9ab64e88a8ec74233d74a544e664635a
lapiscine <geoffroy.pailhebelair@lapiscine.pro>
2019-11-28 11:06:49 +0100

    update projet SF

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Author;
+use App\Form\AuthorType;
 use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -41,7 +44,142 @@ class AuthorController extends AbstractController
             'authorList' => $authorByBio
         ]);
 
+    }
+
+    /**
+     * @Route("/author-form-insert", name="author_form_insert")
+     */
+    public function authorFormInsert(){
+        return $this->render('author/authorFormInsert.html.twig');
+    }
+
+    /**
+     * @Route("/author-insert", name="author_insert")
+     */
+    public function authorInsert(EntityManagerInterface $entityManager, Request $request)
+    {
+
+        $image = $request->request->get('image');
+        $biographie = $request->request->get('biographie');
+        $birthdate = $request->request->get('birthdate');
+        $deathdate = $request->request->get('deathdate'); //nullable
+        $firstname = $request->request->get('firstname');
+        $name = $request->request->get('name');
+
+        $author = new Author();
+
+        $author->setImage($image)
+                ->setBiographie($biographie)
+                ->setBirthdate(new \DateTime($birthdate))
+                ->setDeathdate(new \DateTime($deathdate))
+                ->setFirstname($firstname)
+                ->setName($name);
+
+        $entityManager->persist($author);
+        $entityManager->flush();
+
+        return $this->render('author/authorInsert.html.twig',[
+            'authorInsert' => $author
+        ]);
+    }
+
+    /**
+     * @Route("/author-delete/{id}", name="author_delete")
+     */
+    public function authorDelete(EntityManagerInterface $entityManager, AuthorRepository $authorRepository, $id){
+        $author = $authorRepository->find($id);
+        $entityManager->remove($author);
+        $entityManager->flush();
+
+        return $this->render('author/authorDelete.html.twig');
+    }
+
+    /**
+     * @Route("author-form-update/{id}", name="author_form_update")
+     */
+    public function authorFormUpdate(AuthorRepository $authorRepository, $id){
+        $author = $authorRepository->find($id);
+
+        return $this->render('author/authorFormUpdate.html.twig',[
+           'author' => $author
+        ]);
+    }
+
+    /**
+     * @Route("author-update/{id}", name="author_update")
+     */
+    public function authorUpdate(EntityManagerInterface $entityManager, AuthorRepository $authorRepository, Request
+    $request, $id){
+        $author = $authorRepository->find($id);
+
+        $image = $request->request->get('image');
+        $biographie = $request->request->get('biographie');
+        $birthdate = $request->request->get('birthdate');
+        $deathdate = $request->request->get('deathdate');
+        $name = $request->request->get('name');
+        $firstname = $request->request->get('firstname');
+
+        $author ->setImage($image)
+                ->setDeathdate($deathdate)
+                ->setBiographie($biographie)
+                ->setBirthdate(new \DateTime($birthdate))
+                ->setDeathdate(new \DateTime($deathdate))
+                ->setName($name)
+                ->setFirstname($firstname);
+
+        $entityManager->persist($author);
+        $entityManager->flush();
+
+        return $this->render('author/authorUpdate.html.twig');
+    }
+
+    /**
+     * @Route("/author-insert-form", name="author_insert_form")
+     */
+    public function authorInsertForm(Request $request, EntityManagerInterface $entityManager){
+        $author = new Author();
+        $message = "";
+        $authorForm = $this->createForm(AuthorType::class, $author);
+
+        if($request->isMethod('Post')){
+            $authorForm->handleRequest($request);
+            if($authorForm->isValid()){
+                $entityManager->persist($author);
+                $entityManager->flush();
+                $message = "l'auteur a bien été rajouté";
+            }
+        }
 
 
+        $authorFormView = $authorForm->createView();
+
+        return $this->render('author/authorInsertForm.html.twig',[
+           'authorFormView' => $authorFormView,
+            'message' => $message
+        ]);
+    }
+
+    /**
+     * @Route("/author-update-form/{id}", name="author_update_form")
+     */
+    public function authorUpdateForm(AuthorRepository $authorRepository, EntityManagerInterface $entityManager,
+                                     Request $request, $id){
+        $author = $authorRepository->find($id);
+
+        $authorForm = $this->createForm(AuthorType::class, $author);
+
+        if($request->isMethod('Post')){
+            $authorForm->handleRequest($request);
+            if($authorForm->isValid()){
+                $entityManager->persist($author);
+                $entityManager->flush();
+            }
+        }
+
+        $authorFormView = $authorForm->createView();
+
+        return $this->render('author/authorInsertForm.html.twig', [
+            'authorFormView' => $authorFormView
+        ]);
     }
 }
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,6 +42,139 @@ class BookController extends AbstractController
         $booksByStyle = $bookRepository->getByStyle($word);
         return $this->render('book/bookList.html.twig',[
            'bookList' => $booksByStyle
+        ]);
+    }
+
+    /**
+     * @Route("/book-form-insert", name="book_form_insert")
+     */
+    public function bookFormInsert(){
+        return $this->render('book/bookFormInsert.html.twig');
+    }
+
+    /**
+     * @Route("/book-insert", name="book_insert")
+     */
+    public function bookInsert(EntityManagerInterface $entityManager, Request $request){
+        $image = $request->request->get('image');
+        $inStock = $request->request->get('inStock');
+        $nbPages = $request->request->get('nbPages');
+        $style = $request->request->get('style');
+        $title = $request->request->get('title');
+
+        $book = new Book();
+        $book->getId();
+        $book->setImage($image);
+        $book->setInStock($inStock);
+        $book->setNbPages($nbPages);
+        $book->setStyle($style);
+        $book->setTitle($title);
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->render('book/bookInsert.html.twig',[
+            'bookInsert' => $book
+        ]);
+    }
+
+    /**
+     * @Route("/book-delete/{id}", name="book_delete")
+     */
+    public function bookDelete(EntityManagerInterface $entityManager, BookRepository $bookRepository, $id){
+        $book = $bookRepository->find($id);
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->render('book/bookDelete.html.twig');
+    }
+
+    /**
+     * @Route("/book-form-update/{id}", name="book_form_update")
+     */
+    public function bookFormUpdate(BookRepository $bookRepository, $id){
+        $book = $bookRepository->find($id);
+
+
+        return $this->render('book/bookFormUpdate.html.twig', [
+            'book' => $book
+        ]);
+    }
+
+    /**
+     * @Route("/book-update/{id}", name="book_update")
+     */
+    public function bookUpdate(EntityManagerInterface $entityManager, BookRepository $bookRepository,
+                               Request $request, $id){
+        $book = $bookRepository->find($id);
+
+        $image = $request->request->get('image');
+        $inStock = $request->request->get('inStock');
+        $nbPages = $request->request->get('nbPages');
+        $style = $request->request->get('style');
+        $title = $request->request->get('title');
+
+
+        $book->setImage($image)
+            ->setTitle($title)
+            ->setStyle($style)
+            ->setNbPages($nbPages)
+            ->setInStock($inStock);
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->render('book/bookUpdate.html.twig');
+    }
+
+    /**
+     * @Route("/book-insert-form", name="book_insert_form")
+     */
+    public function bookInsertForm(EntityManagerInterface $entityManager, Request $request){
+        //je crée un nouveau Book,
+        //en créant
+        $book = new Book();
+        $message = "";
+        $bookForm = $this->createForm(BookType::class, $book);
+
+        if($request->isMethod('Post')){
+            $bookForm->handleRequest($request);
+            if($bookForm->isValid()){
+                $entityManager->persist($book);
+                $entityManager->flush();
+                $message = "Le livre a bien été rajouté";
+            }
+        }
+
+        $bookFormView = $bookForm->createView();
+
+        return $this->render('book/bookInsertForm.html.twig',[
+           'bookFormView' => $bookFormView,
+            'message' => $message
+        ]);
+    }
+
+    /**
+     * @Route("/book-update-form/{id}", name="book_update_form")
+     */
+    public function bookUpdateForm(BookRepository $bookRepository, Request $request, $id,
+                                   EntityManagerInterface $entityManager){
+        $book = $bookRepository->find($id);
+
+        $bookForm = $this->createForm(BookType::class, $book);
+
+        if($request->isMethod('Post')){
+            $bookForm->handleRequest($request);
+            if($bookForm->isValid()){
+                $entityManager->persist($book);
+                $entityManager->flush();
+            }
+        }
+
+        $bookFormView=$bookForm->createView();
+
+        return $this->render('book/bookInsertForm.html.twig', [
+            'bookFormView' => $bookFormView
         ]);
     }
 }
